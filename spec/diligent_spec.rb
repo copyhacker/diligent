296666dc9536ae14a198fe5eba9ca977b649e349
Jonathan Vaught <jonathan.vaught@gmail.com>
2014-04-17 15:47:11 -0400

    Generate a hash containing name, author, version and description of gems used in local Gemfile

--- spec/diligent_spec.rb
+++ spec/diligent_spec.rb
@@ -6,10 +6,4 @@ describe Diligent::Info do
       expect(Diligent::Info.legalese).not_to be_nil
     end
   end
-
-  context '.list' do
-    it 'should be a list of gems used' do
-      expect(Diligent::Info.list.first.first).to eq('awesome_print')
-    end
-  end
 end
